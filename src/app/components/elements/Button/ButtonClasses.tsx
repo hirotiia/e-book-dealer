a8111ec064339643c9bd4ff3c77d7a8c7f6945e0
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-08-26 17:21:23 +0900

    :zap: リンクでも使用できるように改良

---
+++ src/app/components/elements/Button/ButtonClasses.tsx
@@ -0,0 +1,35 @@
+import { cva, VariantProps } from 'class-variance-authority';
+import { ReactNode } from 'react';
+
+const button = cva(['cursor-pointer', 'hover:opacity-80', 'flex', 'items-center', 'gap-5'], {
+  variants: {
+    variant: {
+      primary: ['border', 'border-blue-500', 'bg-blue-500', 'text-white'],
+      secondary: ['border', 'border-gray-500', 'bg-gray-500', 'text-white'],
+    },
+    size: {
+      sm: ['px-2', 'py-1', 'text-xs'],
+      md: ['px-4', 'py-2', 'text-sm'],
+      lg: ['px-6', 'py-3', 'text-lg'],
+    },
+    disabled: {
+      true: ['cursor-not-allowed', 'opacity-50'],
+    },
+    rounded: {
+      true: ['rounded'],
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+    rounded: true,
+  },
+});
+
+type ButtonProps = React.ComponentPropsWithRef<'button'> &
+  VariantProps<typeof button> & {
+    as?: 'span';
+    children?: ReactNode;
+  };
+
+export { type ButtonProps, button };
