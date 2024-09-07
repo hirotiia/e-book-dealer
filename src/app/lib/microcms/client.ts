b40edff9f1fef931d0d1d8b01c872987f4fe96af
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-09-07 17:59:34 +0900

    :zap: 型定義方法修正

--- src/app/lib/microcms/client.ts
+++ src/app/lib/microcms/client.ts
@@ -1,13 +1,29 @@
-import { CMSResponse } from '@/types/api/micorcms.types';
-import { createClient } from 'microcms-js-sdk';
+import { createClient, MicroCMSListResponse } from 'microcms-js-sdk';
+import type { MicroCMSDate } from 'microcms-js-sdk';
+
+export type Article = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  description: string;
+  price: number;
+  thumbnail: {
+    url: string;
+    height: number;
+    width: number;
+  };
+} & MicroCMSDate;
 
 const client = createClient({
   serviceDomain: 'e-book-dealer',
   apiKey: process.env.MICROCMS_API_KEY!,
 });
 
-export const getBookItems = async (): Promise<CMSResponse> => {
-  const listData = await client.getList({
+export const getBookItems = async (): Promise<MicroCMSListResponse<Article>> => {
+  const listData = await client.getList<Article>({
     endpoint: 'e-book',
   });
 
