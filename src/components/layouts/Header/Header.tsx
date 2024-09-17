0180e0525aa302de565715a9a7189e3cff088da2
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-09-17 10:56:00 +0900

    :zap: Headerコンポーネント検索ボックス追加

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,12 +1,23 @@
 import { AuthProvider } from '@/app/lib/next-auth/providers/auth-provider';
 import { GloabalNav } from './GlobalNav';
-import { HeaderHeadingLogo } from './HeaderHeadingLogo';
+import Link from 'next/link';
 
 export const Header = () => {
   return (
     <header className="fixed top-0 w-full bg-white/50 shadow-lg backdrop-blur-[20px]">
       <div className="col-start-2 col-end-3 grid grid-cols-2 gap-0">
-        <HeaderHeadingLogo />
+        <div className="flex items-center text-2xl font-bold">
+          <Link href={'/'} className="fade-hover">
+            E-Book dealer
+          </Link>
+        </div>
+        <div className="mx-4 max-w-md grow">
+          <input
+            type="text"
+            placeholder="記事を検索する...."
+            className="w-full rounded-md border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue"
+          />
+        </div>
         <AuthProvider>
           <GloabalNav />
         </AuthProvider>
